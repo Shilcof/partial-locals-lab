fc0dc1e4d7064a9357c9c1755044567736a8efa1
Shilcof <robert.shilcof@gmail.com>
2021-01-07 09:58:56 +0000

    commit tree 586d00c32697625b6c9d45c7dae4045e98e5c77f

--- app/models/student.rb
+++ app/models/student.rb
@@ -13,4 +13,12 @@
 class Student < ActiveRecord::Base
   has_many :classroom_students
   has_many :classrooms, through: :classroom_students
+
+  def self.search(query)
+    if query.size > 0
+      Student.all.where('name like ?', "%#{query}%")  
+    else
+      Student.all
+    end  
+  end
 end
